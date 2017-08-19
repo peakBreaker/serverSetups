4d338425b79a2a5ad654446673eab433d212b37e
Anserio <andershurum@gmail.com>
2017-08-19 18:51:48 +0200

    Ansible now installs needed deps, and copies application to server

--- ansible/run.sh
+++ ansible/run.sh
@@ -3,20 +3,21 @@ if [ $1 = initconfig ]
 then
 # To configure blank server, uncomment the following line:
   echo "Running initial server config!"
-  ansible-playbook config.yml -i hosts --ask-pass -e "user=root"
+  ansible-playbook config.yml -i hosts -e "user=ubuntu" --private-key="/home/anserio/.ssh/myFirstEC2.pem"
 elif [ $1 = config ]
 then
   echo "Running serverconfig"
-  ansible-playbook config.yml -i hosts -K -e "user=grader"
+  # Set user to the sudo user and ssh port to the ssh_port
+  ansible-playbook config.yml -i hosts -e '{"user": "grader", "ansible_port": 2200}' --private-key="/home/anserio/.ssh/id_rsa"
 elif [ $1 = deploy ]
 then
 # If server is configured already, run the following line:
 # First we delete the compiled python files
   echo "Deleting .pyc files"
-  find ../corex_flask -name "*.pyc" -type f -delete
+  find ../../Webapp-Flask/ -name "*.pyc" -type f -delete
   echo "-- OK --"
   echo "Deploying app!"
-  ansible-playbook deploy.yml -i hosts
+  ansible-playbook deploy.yml -i hosts -e '{"user": "grader", "ansible_port": 2200}' --private-key="/home/anserio/.ssh/id_rsa"
 else
-  echo "Please enter a valid command."
+  echo "Please enter a valid command: initconfig | config | deploy"
 fi
